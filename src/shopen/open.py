fd999a9f62d545187a83af27d18d60f118214a33
David Barnett <david@mumind.me>
2024-09-21 15:54:40 -0600

    Raise ValueError for unrecognized operation
    Also fix trivial bug trying to invoke xdg-open on Windows.

--- src/shopen/open.py
+++ src/shopen/open.py
@@ -3,11 +3,19 @@ import subprocess
 import sys
 from typing import Union
 
+SUPPORTED_OPERATIONS = frozenset({"open", "edit"})
+
 
 def open(
     path: Union[os.PathLike, str],
     operation: str = "open",
 ):
+    if operation not in SUPPORTED_OPERATIONS:
+        raise ValueError(
+            f"Unsupported operation {operation!r}. "
+            f"Operation must be one of: {', '.join(SUPPORTED_OPERATIONS)}"
+        )
+
     if hasattr(os, "startfile"):
         os.startfile(path, operation)
         return
@@ -18,9 +26,9 @@ def open(
         from_env = os.environ.get("EDITOR")
         if from_env:
             openers_to_try.append(from_env)
-    if sys.platform != "darwin":
-        openers_to_try.append("xdg-open")
     if sys.platform != "win32":
+        if sys.platform != "darwin":
+            openers_to_try.append("xdg-open")
         openers_to_try.append("open")
 
     for opener in openers_to_try:
--- tests/test_ops.py
+++ tests/test_ops.py
@@ -8,3 +8,9 @@ def test_empty(mocked_openers):
     with pytest.raises(TypeError):
         shopen.open()
     mocked_openers.verify_no_interactions()
+
+
+def test_nonexistent_op(mocked_openers, tmpfile):
+    with pytest.raises(ValueError):
+        shopen.open(tmpfile, "nonexistent-operation")
+    mocked_openers.verify_no_interactions()
